7820b4d5048eeff172afb0affbc8bb9540a3d01a
ricardo soto <ricardosr4@gmail.com>
2023-12-04 00:09:52 -0300

    splashScreen ok

--- app/src/main/java/com/example/multipleapps/MainActivity.kt
+++ app/src/main/java/com/example/multipleapps/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.multipleapps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.multipleapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        screenSplash.setKeepOnScreenCondition{true}
+        val intent = Intent(this, MenuInicio::class.java)
+        startActivity(intent)
+        finish()
+
+
     }
 }
