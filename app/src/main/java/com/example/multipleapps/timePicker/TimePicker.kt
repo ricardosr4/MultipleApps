4f127492a9743804eb17ae4a47a71fe0e5860c5a
ricardo soto <ricardosr4@gmail.com>
2023-12-06 09:01:58 -0300

    se agrega app time picker a esta rama

--- app/src/main/java/com/example/multipleapps/timePicker/TimePicker.kt
+++ app/src/main/java/com/example/multipleapps/timePicker/TimePicker.kt
@@ -3,10 +3,24 @@ package com.example.multipleapps.timePicker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.multipleapps.R
+import com.example.multipleapps.databinding.ActivityTimePickerBinding
 
 class TimePicker : AppCompatActivity() {
+    private lateinit var binding:ActivityTimePickerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_time_picker)
+        binding = ActivityTimePickerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.etTime.setOnClickListener { showTimePickerDialog() }
+    }
+
+    private fun showTimePickerDialog() {
+        val timePicker = TimePickerFragment{onTimeSelected(it)}
+        timePicker.show(supportFragmentManager, "time")
+
+    }
+    private fun onTimeSelected(time:String){
+        binding.etTime.setText("Has Seleccionado las $time Hrs")
     }
 }
